ed1d778f72141ffb1391b494ae5eb31c962ddca8
samueldias99 <112509036+samueldias99@users.noreply.github.com>
2023-02-17 18:47:12 -0300

    resto-dos-exercicios

--- exercicios.js
+++ exercicios.js
@@ -106,31 +106,60 @@ function retornaUltimoElemento(array) {
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
-  const meuArray = [1, 2, 3, 4, 5]
-  return array[0,4]
+  const primeiro = array[0];
+  const ultimo = array[array.length - 1];
 
+  array[0] = ultimo;
+  array[array.length - 1] = primeiro;
+
+  return array;
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
-
+  return string1.toLowerCase() === string2.toLowerCase();
 }
+
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
+  const anoAtual = Number(prompt('Ano Atual: '))
+  const anoNascimento = Number(prompt('Ano de nascimento: '))
+  const anoEmissao = Number(prompt('Ano de emissão CNH: '))
 
+  const idade = anoAtual - anoNascimento;
+  const tempoDeEmissao = anoAtual - anoEmissao;
+
+  const precisaRenovarCincoAnos = idade <= 20 || idade === 20;
+  const precisaRenovarDezAnos = idade > 20 && idade < 50 || idade === 50;
+  const precisaRenovarQuinzeAnos = idade >= 50;
+
+  const renovarCincoAnos = precisaRenovarCincoAnos && tempoDeEmissao >= 5;
+  const renovarDezAnos = precisaRenovarDezAnos && tempoDeEmissao >= 10;
+  const renovarQuinzeAnos = precisaRenovarQuinzeAnos && tempoDeEmissao >= 15;
+
+  const precisaRenovar = renovarCincoAnos || renovarDezAnos || renovarQuinzeAnos;
+
+  console.log(precisaRenovar);
 }
+
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
-
+return ano % 400 == 0 || (ano % 4 == 0 && ano % 100 != 0)
 }
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
   // implemente sua lógica aqui
+  const mais18 = prompt('Voce tem mais de 18 anos?') === 'sim'
+  const ensinoMedioCompleto = prompt ('Possui ensino medio completpo?') === 'sim'
+  const disponibilidade = prompt('Possui disponibilidade Exclusiva?' ) === 'sim'
+
+  const comparar = mais18 && ensinoMedioCompleto && disponibilidade
+  console.log(comparar)
 
 }
