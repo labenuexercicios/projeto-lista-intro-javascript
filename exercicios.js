e3fc01146d8bb25f4dc437ceb84796a66755f7db
alvexmari <mari.silva.ves@gmail.com>
2023-03-24 22:53:49 -0300

    adicionando exercicios

--- exercicios.js
+++ exercicios.js
@@ -20,37 +20,71 @@ function imprimeMensagem() {
 function calculaAreaRetangulo() {
   // implemente sua lógica aqui
 
+let altura = prompt ("Altura do retangulo");
+let largura = prompt ("Largura do retangulo");
+
+const areaDoRetangulo = altura*largura;
+console.log (areaDoRetangulo);
 }
 
 // EXERCÍCIO 02
 function imprimeIdade() {
   // implemente sua lógica aqui
 
+  let anoAtual = prompt ("Informe o ano atual")
+  let anoDeNacimento = prompt ("Informe o seu ano de nascimento")
+
+  const idade = anoAtual - anoDeNacimento
+  console.log (idade)
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
   // implemente sua lógica aqui
 
-}
+ const imc = peso/(altura*altura)
+ return imc
+
+} 
+console.log ("3)", calculaIMC (70,1.70))
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
-
+  
+  let nome = prompt (`Insira o seu nome`);
+  let idade = Number (prompt (`insira a sua idade`));
+  let email = prompt (`insira o seu e-mail`);
+  
+  console.log (`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
 }
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
 
+ listaDeCores = [
+                       "Azul",
+                       "Amarelo", 
+                       "Vermelho"
+                      ];
+
+ const primeiraCor = listaDeCores [0]; // verde
+ const segundaCor = listaDeCores [1]; //roxo
+ const terceiraCor = listaDeCores [2]; // lilas
+
+ console.log (listaDeCores)
+
 }
+
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
 
+  const saudacao = "oi"
+  console.log (saudacao.toUpperCase());
 }
 
 // EXERCÍCIO 07
@@ -75,6 +109,8 @@ function retornaPrimeiroElemento(array) {
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui
 
+  array [array.length-1]
+  return array
 }
 
 // EXERCÍCIO 11
