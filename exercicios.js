b6ec0af523acf0d5679abe258e62d0a4b44662e6
Rafael Simas <rafaelsimasdev@gmail.com>
2023-05-14 11:12:12 -0300

    fiz ate a 14.

--- exercicios.js
+++ exercicios.js
@@ -18,88 +18,161 @@ function imprimeMensagem() {
 
 // EXERCÍCIO 01
 function calculaAreaRetangulo() {
-  // implemente sua lógica aqui
+  const altura = Number(prompt(`Digite sua altura`))
+  const largura = Number(prompt(`Digite sua altura`))
+  const area = altura * largura
 
+  console.log(area)
 }
 
 // EXERCÍCIO 02
 function imprimeIdade() {
-  // implemente sua lógica aqui
+  const anoAtual = Number(prompt(`Digite o ano em que estamos`))
+  const anoDeNascimento = Number(prompt(`Digite o seu ano de nascimento`))
+  const idade = anoAtual - anoDeNascimento
 
+  console.log(idade)
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
-  // implemente sua lógica aqui
+  // let peso = 85
+  // let altura = 1.8 
+  let IMC = peso / ( altura * altura)
 
+  return IMC
 }
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
-  // implemente sua lógica aqui
-  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+  let nome = prompt(`Digite seu nome`)
+  let idade = Number(prompt(`Digite sua idade`))
+  let email = prompt(`Digite seu e-mail`)
+
+  console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
 
 }
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
-  // implemente sua lógica aqui
+  let cor1 =  prompt("Qual sua cor predileta?")
+  let cor2 =  prompt("Qual sua cor predileta?")
+  let cor3 =  prompt("Qual sua cor predileta?")
+
+  const cor = [cor1, cor2, cor3]
+  
+  console.log (cor)
 
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
-  // implemente sua lógica aqui
 
+  return string.toUpperCase()
+  
 }
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
-  // implemente sua lógica aqui
+  let paraLucrar = (custo / valorIngresso)
+
+  return paraLucrar
 
 }
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
-  // implemente sua lógica aqui
+
+  return string1.length === string2.length
 
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
-  // implemente sua lógica aqui
+  
+  return array[0]
 
 }
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
-  // implemente sua lógica aqui
+  return array[array.length -1]
+
 
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
-  // implemente sua lógica aqui
+  let num1 = array[0]
+  let num2 = array[array.length -1]
+
+  array[0] = num2
+  array[array.length -1] = num1
+
+  return array
 
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
-  // implemente sua lógica aqui
-
+ 
+  return string1.toUpperCase() === string2.toUpperCase()
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
-  // implemente sua lógica aqui
+  let anoAtual = Number(prompt("Em que ano estamos?"))
+  let anoDeNascimento = Number(prompt("Qual seu ano de nascimento"))
+  let emissaoRg = Number(prompt("Quando seu Rg foi emitido"))
+  let idade = anoAtual - anoDeNascimento
+  let tempoRg = anoAtual - emissaoRg
+
+  // if (idade <= 20) {
+  //   if (tempoRg >= 5) {
+  //     console.log(true);
+  //   } else {
+  //     console.log(false);
+  //   }
+  // } else if (idade <= 50) {
+  //   if (tempoRg >= 10) {
+  //     console.log(true);
+  //   } else {
+  //     console.log(false);
+  //   }
+  // } else if (idade > 50){
+  //   if (tempoRg >= 15){
+  //     console.log(true)
+  //   } else {
+  //     console.log(false)
+  //   }
+  // }
+
+
+  //outra forma de fazer
+
+  let num1 = idade <= 20 && tempoRg >= 5
+  let num2 = idade <= 50 && tempoRg >= 10
+  let num3 = idade > 50 && tempoRg >= 15
+
+  console.log ( num1 || num2 || num3 )
 
 }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
-  // implemente sua lógica aqui
+
+  if (ano % 400 === 0) {
+    return true;
+  } else if (ano % 100 === 0) {
+    return false;
+  } else if (ano % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 
 }
+console.log(checaAnoBissexto(1997))
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
