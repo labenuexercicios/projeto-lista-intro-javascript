207b5d581e0ab6b3798cac6de26cd9f1e0602d20
VitorPaes <vitornoepaes@hotmail.com>
2023-07-19 00:36:30 -0300

    EXERCICIO 13

--- exercicios.js
+++ exercicios.js
@@ -28,25 +28,27 @@ function imprimeIdade() {
   //   implemente sua lógica aqui
   const anoAtual = prompt("Digite a altura");
   const anoDeNascimento = prompt("Digite a largura");
-  const idade = anoAtual - anoDeNascimento
-  console.log(idade)
+  const idade = anoAtual - anoDeNascimento;
+  console.log(idade);
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
   // implemente sua lógica aqui
-  const IMC = peso / (altura * altura)
-  return IMC
+  const IMC = peso / (altura * altura);
+  return IMC;
 }
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
-  const nome = prompt("Digite o seu nome: ")
-  const idade = prompt("Digite o seu idade: ")
-  const email = prompt("Digite o seu email: ")
-  console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
+  const nome = prompt("Digite o seu nome: ");
+  const idade = prompt("Digite o seu idade: ");
+  const email = prompt("Digite o seu email: ");
+  console.log(
+    `Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`
+  );
 }
 
 // EXERCÍCIO 05
@@ -55,62 +57,76 @@ function imprimeTresCoresFavoritas() {
   const cor1 = prompt("Digite sua cor favorita: ");
   const cor2 = prompt("Digite sua cor favorita: ");
   const cor3 = prompt("Digite sua cor favorita: ");
-  const cores = [cor1,cor2,cor3]
-  console.log (cores)
+  const cores = [cor1, cor2, cor3];
+  console.log(cores);
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
-  const toUpperCase = string.toUpperCase()
-  return toUpperCase
+  const toUpperCase = string.toUpperCase();
+  return toUpperCase;
 }
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
   // implemente sua lógica aqui
-  return custo / valorIngresso
+  return custo / valorIngresso;
 }
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
-  return string1.length === string2.length
+  return string1.length === string2.length;
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
   // implemente sua lógica aqui
-  return array[0]
+  return array[0];
 }
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui
-  return array.pop()
+  return array.pop();
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
-  const primeiroElemento = array[0]
-  const ultimoElemento = array[array.length -1]
-  
-  array[0] = ultimoElemento;
-  array[array.length -1] = primeiroElemento;
+  const primeiroElemento = array[0];
+  const ultimoElemento = array[array.length - 1];
 
-  return array
+  array[0] = ultimoElemento;
+  array[array.length - 1] = primeiroElemento;
+
+  return array;
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
-  return Boolean(string1.toUpperCase() === string2.toUpperCase())
+  return Boolean(string1.toUpperCase() === string2.toUpperCase());
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
+  const anoAtual = prompt("Digite o ano atual: ");
+  const anoDeNascimento = prompt("Digite o ano de nascimento: ");
+  const anoDeAdmissao = prompt("Digite o ano de admissao: ");
+  if ((anoAtual - anoDeNascimento) <= 20){
+    const precisaRenovar = (anoAtual - anoDeAdmissao) >= 5;
+    console.log(Boolean(precisaRenovar))
+  }else if ((anoAtual - anoDeNascimento) > 20 && (anoAtual - anoDeNascimento) <= 50){
+    const precisaRenovar = (anoAtual - anoDeAdmissao) >= 10
+    console.log(Boolean(precisaRenovar))
+  } else if((anoAtual - anoDeNascimento) > 50){
+    const precisaRenovar = (anoAtual - anoDeAdmissao) > 15
+    console.log(Boolean(precisaRenovar))
+  }
+
 }
 
 // EXERCÍCIO 14
