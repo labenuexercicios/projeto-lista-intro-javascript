c4db32fc33e41b188bf8f2f73c134b91fd214a8f
Kenia Araujo <kenia.kfa@gmail.com>
2023-11-26 16:08:06 -0600

    Projeto Intro JS

--- exercicios.js
+++ exercicios.js
@@ -17,92 +17,207 @@ function imprimeMensagem() {
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
-function calculaAreaRetangulo() {
-  // implemente sua lógica aqui
+
+function calculaAreaRetangulo(){
+  const altura= parseFloat(prompt("Digite a altura do retangulo")); 
+  const largura= parseFloat(prompt("Digite a largura do retangulo"));
+
+   const area= altura*largura;
+
+   console.log(`A área do retângulo é: ${area}`);
+
 
 }
+
+calculaAreaRetangulo();
 
 // EXERCÍCIO 02
-function imprimeIdade() {
-  // implemente sua lógica aqui
-
-}
+function imprimeIdade(){
+  const anoAtual=parseInt(prompt("Qual e o ano atual"));
+  const anoDeNascimento= parseInt(prompt("Qual e o ano que voce nasceu"));
+ 
+   const idade= anoAtual-anoDeNascimento;
+   console.log(`A sua idade é: ${idade}`);
+ }
+ 
+ imprimeIdade();
 
 // EXERCÍCIO 03
-function calculaIMC(peso, altura) {
-  // implemente sua lógica aqui
-
-}
+function calculaIMC(){
+  const peso=parseFloat(prompt("Qual e o seu peso"));
+  const altura=parseFloat(prompt("Qual e a sua altura"));
+ 
+  const imc= peso/altura;
+  console.log(imc.toFixed(1))
+ 
+  
+ }
+ 
+ calculaIMC()
 
 // EXERCÍCIO 04
-function imprimeInformacoesUsuario() {
-  // implemente sua lógica aqui
-  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
 
-}
+  function imprimeInformacoesUsuario(){
+    const name=prompt(("Qual e o seu nome"));
+    const idade=prompt(("Qual e a sua idade"));
+    const email=prompt((" Qual e o seu email"));
+    
+    console.log( "Meu nome é",{name},"tenho", {idade}, "anos", "e o meu email é", {email});
+    }
+    
+    imprimeInformacoesUsuario()
 
 // EXERCÍCIO 05
-function imprimeTresCoresFavoritas() {
-  // implemente sua lógica aqui
-
+function imprimeTresCoresFavoritas(){
+  const cor1= prompt(("Qual e a sua cor primeira preferida"));
+  const cor2= prompt(("Qual e a sua cor segunda preferida"));
+  const cor3= prompt(("Qual e a sua cor  terceira preferida"));
+  
+  const cores=[cor1,cor2,cor3];
+  return cores;
 }
+
+
+imprimeTresCoresFavoritas()
 
 // EXERCÍCIO 06
-function retornaStringEmMaiuscula(string) {
-  // implemente sua lógica aqui
-
-}
+function retornaStringEmMaiuscula(){
+  const name= "Kenia Araujo";
+ 
+  console.log(name.toUpperCase());
+ }
+ 
+ retornaStringEmMaiuscula()
 
 // EXERCÍCIO 07
-function calculaIngressosEspetaculo(custo, valorIngresso) {
-  // implemente sua lógica aqui
-
+function calculaIngressoEspetaculo(){
+  const custo=prompt(("Qual e o custo"));
+  const ingressoValor= prompt(("Qual e o valor do ingresso"));
+  
+  const lucro= custo/ingressoValor;
+  return lucro;
 }
+calculaIngressoEspetaculo()
 
 // EXERCÍCIO 08
-function checaStringsMesmoTamanho(string1, string2) {
-  // implemente sua lógica aqui
+function checaStringsMesmoTamanho(){
+  const palavra1= prompt(("Digite uma palavra"));
+  const palavra2= prompt(("Digite outra palavra"));
 
+  
+ return palavra1.length === palavra2.length;
+ 
+  
 }
+checaStringsMesmoTamanho();
 
 // EXERCÍCIO 09
-function retornaPrimeiroElemento(array) {
-  // implemente sua lógica aqui
-
-}
+  function retornaPrimeiroElemento(){
+  const elemento1= prompt(("Preenche a primeira posicao"));
+  const elemento2= prompt(("Preenche a segunda posicao"));
+  const elemento3= prompt(("Preenche a terceira posicao"));
+ 
+   const posicao1=[elemento1,elemento2, elemento3];
+      return posicao1[0];
+ 
+ }
+ 
+ retornaPrimeiroElemento();
 
 // EXERCÍCIO 10
-function retornaUltimoElemento(array) {
-  // implemente sua lógica aqui
-
-}
+function retornaPrimeiroElemento(){
+  const elemento1= prompt(("Preenche a primeira posicao"));
+  const elemento2= prompt(("Preenche a segunda posicao"));
+  const elemento3= prompt(("Preenche a terceira posicao"));
+ 
+   const posicao1=[elemento1,elemento2, elemento3];
+      return posicao1[2];
+ 
+ }
+ 
+ retornaPrimeiroElemento();
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
-  // implemente sua lógica aqui
-
+  [array[0], array[array.length - 1]] = [array[array.length - 1], array[0]];
+    
+    return array;
+    
+    
 }
+const elementos=["one", "two", "three","four","five"];
+
+console.log(trocaPrimeiroEUltimo(elementos));
+
 
 // EXERCÍCIO 12
-function checaIgualdadeDesconsiderandoCase(string1, string2) {
-  // implemente sua lógica aqui
+function checaIgualdadeDesconsiderandoCase(){
+   
+  const palavra1= prompt(("Digite uma palavra"));
+  const palavra2= prompt(("Digite outra palavra"));
+
+  
+ return palavra1.toLowerCase() === palavra2.toLowerCase();
+ 
 
 }
+
+checaIgualdadeDesconsiderandoCase()
+
 
 // EXERCÍCIO 13
-function checaRenovacaoRG() {
-  // implemente sua lógica aqui
+function checaRenovacaoRG(){
+  const anoAtual= parseInt(prompt("Digite o ano atual"));
+  const anoNascimento=parseInt(prompt("Digite ano de nascimento"));
+  const anoEmissaoRG=parseInt(prompt("Digite ano de Emissao do RG"));
+ 
+  const idade= anoAtual-anoNascimento
+  const tempoDesdeEmissao = anoAtual - anoEmissaoRG;
+     if (idade <= 20 || idade === 20) {
+         return tempoDesdeEmissao >= 5;
+     } else if (idade > 20 && idade <= 50 || idade === 50) {
+         return tempoDesdeEmissao >= 10;
+     } else {
+         return tempoDesdeEmissao >= 15;
+ 
+         
+     }
+ }
+ 
+ const precisaRenovar = checaRenovacaoRG();
+ console.log("Precisa renovar o RG:", precisaRenovar);
 
-}
+
 
 // EXERCÍCIO 14
-function checaAnoBissexto(ano) {
-  // implemente sua lógica aqui
 
+function checaAnoBissexto() {
+
+  const ano=parseInt(prompt("Digite o ano atual"))
+
+  if ((ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0)) {
+      return true;
+  } else {
+      return false;
+  }
 }
+
+
+checaAnoBissexto();
+
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
-  // implemente sua lógica aqui
+  // Solicitar informações ao usuário
+  const idade = prompt("Você tem mais de 18 anos? (responda sim ou nao)").toLowerCase();
+  const ensinoMedioCompleto = prompt("Você possui ensino médio completo? (responda sim ou nao)").toLowerCase();
+  const disponibilidadeCurso = prompt("Você possui disponibilidade exclusiva durante os horários do curso? (responda sim ou nao)").toLowerCase();
+// Verificar se todas as respostas são "sim"
+  const inscricaoValida = idade === "sim" && ensinoMedioCompleto === "sim" && disponibilidadeCurso === "sim";
 
+  // Exibir o resultado no console
+  console.log("Inscrição válida para o curso da Labenu:", inscricaoValida);
 }
+
+// Chamar a função para testar
+checaValidadeInscricaoLabenu();
